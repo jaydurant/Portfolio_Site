d148b6622872133d510798c339a394e754b1578e
jdurant <durantjay1@gmail.com>
2015-11-20 15:23:18 -0800

    Added comments to all files to explain structure and the why certain features were utilized

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,6 @@
+/* This function listens for a user to click event on a link which has the href containg a '#' wehere a animation 
+	occurs where the top of user scroller is then moved to the top of target elemtn with the offSet().top method
+*/
 $(function() {
 	  $('a[href*=#]:not([href=#])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
